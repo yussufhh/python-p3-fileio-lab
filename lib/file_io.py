477a60c1d6437c999b5d0c60820ca2163c051614
yussufhh <0793477632g@gmail.com>
2024-08-22 08:15:35 +0300

    final-commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
-def write_file(file_name, file_content):
-    pass
+# lib/file_io.py
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    """Writes content to a .txt file. Creates a new file or overwrites an existing file."""
+    file_name_with_ext = str(file_name) + ".txt"
+    with open(file_name_with_ext, 'w') as file:
+        file.write(file_content)
+
+
+def append_file(file_name, file_content):
+    """Appends content to a .txt file. Creates the file if it does not exist."""
+    file_name_with_ext = str(file_name) + ".txt"
+    with open(file_name_with_ext, 'a') as file:
+        file.write(file_content)
+
 
 def read_file(file_name):
-    pass
+    """Reads content from a .txt file and returns it."""
+    file_name_with_ext = str(file_name) + ".txt"
+    with open(file_name_with_ext, 'r') as file:
+        content = file.read()
+    return content
